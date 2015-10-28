ba092197a6ac49fe09debd5fe7572a769fc043de
Polariche <polariche@gmail.com>
2015-10-28 18:12:12 +0900

    캐릭터 움직이는 스크립트

---
+++ Assets/Scripts/move.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	public float speed;
+
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		float sero = Input.GetAxis ("Vertical");
+		float garo = Input.GetAxis ("Horizontal");
+		Vector3 dir = new Vector3 (garo, sero, 0);
+
+		transform.Translate (dir * speed * Time.deltaTime);
+
+	}
+}
