e97af305769d489ad7a5e59f9f24b201bfbef0db
leesuyun <2soooy@naver.com>
2015-11-04 17:13:07 +0900

    move

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	void Move () {
+		transform.Translate (transform.InverseTransformDirection (dir)* speed * Time.deltaTime);
+		transform.rotation = Quaternion.FromToRotation (Vector3.right, dir);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+
+	void Start () {
+
+	}
+	// Update is called once per frame
+	void Update () {
+		float sero = Input.GetAxis ("Vertical");
+		float garo = Input.GetAxis ("Horizontal");
+		Vector3 dir = new Vector3 (garo, sero, 0);
+
+	}
+}
