4f3eae25e1cb11927f432dd0a7178f4708d027ef
Polariche <polariche@gmail.com>
2015-11-15 13:56:22 +0900

    Rewrite GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,39 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using System;
 
-public enum GameState
-{
-	Ready,
-	Play,
-	End
+public enum GameState { SPLASH, READY, PLAY, PLAY_PAUSE, END, CONFIG }
+
+public delegate void OnStateChangeHandler(); 
+
+public class GameManager : UnityEngine.Object {
+    protected GameManager() { }
+    public static GameManager instance = null;
+    public event OnStateChangeHandler OnStateChange;
+    public GameState gameState { get; private set; }
+    
+
+    public static GameManager Instance {
+        get {
+            if (GameManager.instance == null) {
+                DontDestroyOnLoad(GameManager.instance);
+                GameManager.instance = new GameManager();
+            }
+            return GameManager.instance;
+        }
+    }
+
+
+    public void SetGameState(GameState state) {
+        this.gameState = state;
+        OnStateChange();
+    }
+
+
+    public void OnApplicationQuit() {
+        GameManager.instance = null;
+    }
+
 }
 
-public class GameManager : MonoBehaviour {
-
-	public GameState Gs;
-	public float Time;
-	public GameObject FinishGUI;
-	public GUIText Final_Count_Time;
-	public GUIText Final_Count_HP;
-	public GUIText Final_Count_Item;
-	public GUIText Final_Score;
-	public int Score;
-	//tip,prefab
-	void Start(){
-
-	}
-}
+//Credit goes to https://www.packtpub.com/books/content/creating-simple-gamemanager-using-unity3d
