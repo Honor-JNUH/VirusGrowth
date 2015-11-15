f3f04ff7f35abd8a2afb584bf24691d8e09f7c64
Polariche <polariche@gmail.com>
2015-11-15 16:52:19 +0900

    Create something unholy
    Just a filler 'game' created to test game manager. This is what happens when I listen to Flowey's battle theme for 2 hours

---
+++ Assets/Scripts/COOKIES.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class COOKIES : MonoBehaviour {
+    public Button CookieButton;
+    public GameObject IDEACOOKIE;
+    public const float ScreenWidth = 800;
+    public const float ScreenHeight = 600;
+
+
+    void SPAWNCOOKIE() {
+        //ĹE͜ T͟ ̛     T́H ҉E R ̴E ̢ ͡B̨ E͞ ̧   C O O K͏I͘ ͞E
+
+        Instantiate(IDEACOOKIE, RandomizeLocation(), Quaternion.identity);
+        GameManager.instance.AddScore(1);
+    }
+
+    Vector3 RandomizeLocation() {
+        return new Vector3(Random.Range(0,ScreenWidth), Random.Range(0, ScreenHeight), 0);
+    }
+
+    void Start()
+    {
+        CookieButton.onClick.AddListener(() => { SPAWNCOOKIE(); });
+    }
+}
