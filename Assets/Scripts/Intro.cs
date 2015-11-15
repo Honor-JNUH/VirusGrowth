78fd87d09e87965d33e068b755b60743636285b5
Polariche <polariche@gmail.com>
2015-11-15 16:45:05 +0900

    Update Intro.cs

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -3,45 +3,40 @@ using System.Collections;
 
 public class Intro : MonoBehaviour {
     GameManager GM;
-    
-    public void HandleOnStateChange()
-    {
-        Debug.Log("OnStateChange!");
+
+    public void HandleOnStateChange() {
+    }
+
+    public void StartGame() {
+        GM.ResetRun();
+        Debug.Log(GM.Level);
+
+        GM.SetGameState(GameState.READY);
+        Debug.Log(GM.gameState);
+
         Application.LoadLevel("ready");
     }
 
-    public void StartGame()
-    {
-        //start game scene
-        GM.SetGameState(GameState.READY);
-        Debug.Log(GM.gameState);
-    }
-
-    public void Quit()
-    {
+    public void Quit() { 
         Debug.Log("Quit!");
         Application.Quit();
     }
-    
-    void Awake()
-    {
-        GM = GameManager.Instance;
+
+    void Start() {
+        GM = GameManager.instance;
         GM.OnStateChange += HandleOnStateChange;
     }
 
-    public void OnGUI()
-    {
-        //menu layout
+    public void OnGUI() {
         GUI.BeginGroup(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 800));
         GUI.Box(new Rect(0, 0, 100, 200), "Menu");
+
         if (GUI.Button(new Rect(10, 40, 80, 30), "Start"))
-        {
             StartGame();
-        }
+
         if (GUI.Button(new Rect(10, 160, 80, 30), "Quit"))
-        {
             Quit();
-        }
+
         GUI.EndGroup();
     }
 }
