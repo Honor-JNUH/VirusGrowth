2ba5ffde79992f266af0c6a064d2310b738ef78e
Polariche <polariche@gmail.com>
2015-11-15 16:46:30 +0900

    Create Play.cs

---
+++ Assets/Scripts/Play.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Play : MonoBehaviour
+{
+    GameManager GM;
+    public Button NextButton; //DEMO ONLY; Various winning/losing conditions should be implemented for the final game
+
+    public void HandleOnStateChange() { 
+    }
+
+    public void EndPlaying()
+    {
+        Debug.Log("End of Level " + GM.Level);
+        GM.SetGameState(GameState.END);
+        Application.LoadLevel("end");
+    }
+
+    void Awake()
+    {
+        GM = GameManager.instance;
+        GM.OnStateChange += HandleOnStateChange;
+    }
+
+    void Start()
+    {
+        if (NextButton != null)
+        {
+            NextButton.onClick.AddListener(() => { EndPlaying(); });
+        }
+    }
+
+}
