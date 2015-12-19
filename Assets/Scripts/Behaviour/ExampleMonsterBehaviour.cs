44abb163e1a59cf643bb5660cf22c9784a228831
Polariche <polariche@gmail.com>
2015-12-19 23:31:53 +0900

    Add monster behaviour

---
+++ Assets/Scripts/Behaviour/ExampleMonsterBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleMonsterBehaviour : MonoBehaviour {
+
+    public Movement movement;
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
