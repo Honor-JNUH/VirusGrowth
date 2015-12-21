bb05764889df5d14c97b23d718ec7413cb5ee17a
Polariche <polariche@gmail.com>
2015-12-21 19:48:47 +0900

    Add CameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 playerPos { get { return player.transform.position; } }
+    Vector3 offset;
+    Vector3 playerOffset;
+
+    void Start()
+    {
+        offset = transform.position;
+        playerOffset = playerPos;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = offset + playerPos - playerOffset;
+	}
+}
