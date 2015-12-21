bc36ba6d1717a3d9cb4ef576d3ad9e514e186b0c
Polariche <polariche@gmail.com>
2015-12-21 14:00:51 +0900

    BulletBehaviour becomes child class of WeaponBehaviour

--- Assets/Scripts/Behaviour/BulletBehaviour.cs
+++ Assets/Scripts/Behaviour/BulletBehaviour.cs
@@ -1,36 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public abstract class BulletBehaviour : MonoBehaviour {
+public abstract class BulletBehaviour : WeaponBehaviour {
 
     public float defaultSpeed;
-    public float defaultDamage;
-    public String targetTag;
-
     public float speed { get; protected set; }
-    public float damage { get; protected set; }
 
-    protected string targetTag;
-
-    void Start () 
-    {
-        speed = defaultSpeed;
-        damage = defaultDamage;
-    }
 
     protected virtual void Fire() 
     {
         transform.Translate(transform.forward * speed * Time.deltaTime);
     }
-
-    protected virtual void Hit(GameObject o) 
-    {
-        if (o.tag == targetTag)
-            o.SendMessage("ApplyDamage", damage);
-    }
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        Hit(collision.gameObject);
-    }
+    
 }
---
+++ Assets/Scripts/Behaviour/WeaponBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class WeaponBehaviour : MonoBehaviour {
+
+    public float defaultDamage;
+    public String targetTag;
+
+    public float damage { get; protected set; }
+
+    protected string targetTag;
+
+    void Start () 
+    {
+        speed = defaultSpeed;
+        damage = defaultDamage;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        HandleCollision(collision);
+    }
+
+    protected virtual void Hit(GameObject o) 
+    {
+        o.SendMessage("ApplyDamage", damage);
+    }
+
+    protected virtual void HandleCollision(Collision2D collision)
+    {
+        GameObject o = collision.gameObject;
+        if (o.tag == targetTag)
+            Hit(o);
+    }
+
+    
+}
