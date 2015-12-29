91f68231077e8b630359ed663d67e747716a9ad2
Polariche <polariche@gmail.com>
2015-12-29 16:34:49 +0900

    Add click test

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clicker : MonoBehaviour
+{
+    public GameObject circlePrefab;
+    GameObject circle;
+    Vector3 inputPos { get { Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition); pos.z = 0; return pos; } }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            circle = (GameObject) Instantiate(circlePrefab, inputPos, Quaternion.FromToRotation(Vector3.up, Vector3.up));
+        }
+
+        if (Input.GetMouseButton(0)) {
+            Vector3 pos = circle.transform.position;
+            circle.transform.Translate(inputPos - pos);
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            Destroy(circle);
+        }
+    }
+}
