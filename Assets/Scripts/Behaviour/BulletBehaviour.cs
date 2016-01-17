69459218ecf3ea5c15b5d2766d91d89aebc6d010
Polariche <polariche@gmail.com>
2016-01-17 16:02:37 +0900

    Update BulletBehaviour.cs

--- Assets/Scripts/Behaviour/BulletBehaviour.cs
+++ Assets/Scripts/Behaviour/BulletBehaviour.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class BulletBehaviour : WeaponBehaviour {
 
-    public float defaultSpeed;
-
     void Awake () {
         damage = defaultDamage;
         speed = defaultSpeed;
